ab8aa6e6ff39dec2835eb28218d145c6daa23cf0
madelynntuttle <hello@madelynntuttle.com>
2016-09-24 21:53:50 +0900

    Don't assign vars to window

--- _assets/scripts/main.js
+++ _assets/scripts/main.js
@@ -1,16 +1,27 @@
-var logoFiles = ["/assets/images/logo1.png", "/assets/images/logo2.png", "/assets/images/logo3.png", "/assets/images/logo4.png", "/assets/images/logo5.png", "/assets/images/logo6.png", "/assets/images/logo7.png", "/assets/images/logo8.png", "/assets/images/logo9.png", "/assets/images/logo10.png"];
-            
-var randomNumberPrev = 0;
+(function () {
+    var logoFiles = ["/assets/images/logo1.png", 
+                     "/assets/images/logo2.png", 
+                     "/assets/images/logo3.png", 
+                     "/assets/images/logo4.png", 
+                     "/assets/images/logo5.png", 
+                     "/assets/images/logo6.png", 
+                     "/assets/images/logo7.png", 
+                     "/assets/images/logo8.png", 
+                     "/assets/images/logo9.png", 
+                     "/assets/images/logo10.png"];
 
-var logoTimer = function () {
-    var randomNumber = Math.floor(Math.random() * 10); 
-    if (randomNumber === randomNumberPrev) {
-        randomNumber = (randomNumber + 1) % 10;
-    }
-    var t = logoFiles[randomNumber];
+    var randomNumberPrev = 0;
 
-    document.getElementById("logo").src = t;
-    randomNumberPrev = randomNumber;
-};
+    var logoTimer = function () {
+        var randomNumber = Math.floor(Math.random() * 10); 
+        if (randomNumber === randomNumberPrev) {
+            randomNumber = (randomNumber + 1) % 10;
+        }
+        var t = logoFiles[randomNumber];
 
-var changeLogo = setInterval(logoTimer, 1000);
+        document.getElementById("logo").src = t;
+        randomNumberPrev = randomNumber;
+    };
+
+    var changeLogo = setInterval(logoTimer, 1000);
+})(document);
